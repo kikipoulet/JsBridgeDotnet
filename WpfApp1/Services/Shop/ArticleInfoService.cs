4a346d79a0c1a7f1fc25fbe74886438c7957ed5b
Clément Sepulchre <sepulchreclement.dic@gmail.com>
2026-01-08 17:05:03 +0100

    articleinfo

---
+++ WpfApp1/Services/Shop/ArticleInfoService.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WpfApp1.Services.Shop;
+
+public partial class ArticleInfoService : ObservableObject
+{
+    [ObservableProperty] private Article _article;
+    
+    public ArticleInfoService(string id)
+    {
+        Article = MainWindow.serviceProvider.GetRequiredService<ArticleListService>().Articles.FirstOrDefault(a => a.Id == id);
+    }
+}
